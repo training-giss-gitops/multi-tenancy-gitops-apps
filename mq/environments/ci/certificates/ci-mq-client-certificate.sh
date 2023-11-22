1f1ca1d8b565a89c15bb6accf9d7de3821f80950
Jesus Almaraz <jesus.mah@gmail.com>
2023-11-22 15:44:51 +0100

    removing uneeded files

--- mq/environments/ci/certificates/ci-mq-client-certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CLUSTER_DOMAIN=$(oc get dns cluster -o jsonpath='{ .spec.baseDomain }')
-
-# Create Kubernetes Secret yaml
-( echo "cat <<EOF" ; cat ci-mq-client-certificate.yaml_template ; echo EOF ) | \
-CLUSTER_DOMAIN=${CLUSTER_DOMAIN} \
-sh > ci-mq-client-certificate.yaml
--- mq/environments/ci/certificates/ci-mq-server-certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CLUSTER_DOMAIN=$(oc get dns cluster -o jsonpath='{ .spec.baseDomain }')
-
-# Create Kubernetes Secret yaml
-( echo "cat <<EOF" ; cat ci-mq-server-certificate.yaml_template ; echo EOF ) | \
-CLUSTER_DOMAIN=${CLUSTER_DOMAIN} \
-sh > ci-mq-server-certificate.yaml
